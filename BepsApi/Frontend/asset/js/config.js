adae7f4282af66773ef12bd78633b59c98785f71
박승우우 <b23038.tdc.hanmacgroup@gmail.com>
2025-11-04 14:15:16 +0900

    Update IP address to 172.16.40.192

--- BepsApi/Frontend/asset/js/config.js
+++ BepsApi/Frontend/asset/js/config.js
@@ -1,7 +1,7 @@
-const baseUrl = "http://172.16.8.216:20000/";
+const baseUrl = "http://172.16.40.192:20000/";
 window.baseUrl = baseUrl;
 
-window.websocketUrl="ws://172.16.8.216:20000/websocket/";
+window.websocketUrl="ws://172.16.40.192:20000/websocket/";
 
 // config.js 맨 아래에 추가
 console.log("✅ config.js loaded. baseUrl =", window.baseUrl);
